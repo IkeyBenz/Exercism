37ea364692d057e985762cb96b333a792b4617a1
Ikey Benzaken <ikey.benz@gmail.com>
2019-02-21 15:15:57 -0800

    Made difference-of-squares more efficient

--- go/difference-of-squares/difference_of_squares.go
+++ go/difference-of-squares/difference_of_squares.go
@@ -2,13 +2,13 @@ package diffsquares
 
 // SquareOfSum takes an integer, n, and returns the sum of all numbers from 0-n squared
 func SquareOfSum(n int) int {
-	return ((n * (n + 1)) / 2) * ((n * (n + 1)) / 2)
+	sum := (n * (n + 1)) / 2
+	return sum * sum
 }
 
 // SumOfSquares takes an integer, n, and returns the sum of squares of numbers from 0-n
 func SumOfSquares(n int) int {
-	// An^3 + Bn^2 + Cn
-	return 1
+	return (n * (n + 1) * ((2 * n) + 1)) / 6
 }
 
 // Difference returns the difference between SquareOfSum(n) and SumOfSquares(n)
